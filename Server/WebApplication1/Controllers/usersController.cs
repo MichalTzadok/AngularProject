71105d82d21ffdc98f1a609d1dd13a53476d5098
MichalTzadok <michal9741003@gmail.com>
2024-04-13 21:39:52 +0300

    Switched data source to JSON files

--- Server/WebApplication1/Controllers/usersController.cs
+++ Server/WebApplication1/Controllers/usersController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Text.Json;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using WebApplication1.Models;
 
@@ -10,57 +12,51 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-       private static List<User> users = new List<User>()
-            {
-                new User { Id = 1, Name = "Michal", Password = "michal123", SearchJobField = "Software_Development",JobCount=0 },
-                new User { Id = 2, Name = "Tamar", Password = "tamar123", SearchJobField = "Healthcare",JobCount=0 },
-                new User { Id = 3, Name = "Elchanan", Password = "elchanan456", SearchJobField = "Finance" ,JobCount=0},
-                new User { Id = 4, Name = "Avital", Password = "avital789", SearchJobField = "Engineering",JobCount=0 },
-                new User { Id = 5, Name = "Inon", Password = "inon321", SearchJobField = "Retail_and_Sales" ,JobCount=0}
-            };
-       
-// GET api/user/authenticate
-       [HttpGet("{name}/{password}")]
-        public  IActionResult Authenticate(string name,string password)
-        {
-          var user=  users.FirstOrDefault(u => u.Name.Equals(name) && u.Password.Equals(password));
-        if(user==null)
-        return NotFound();
-        return Ok(user);
-        }
-         [HttpPut]    
-         public void UpdateUser(int userId)
-        {
-            User user = users.FirstOrDefault(u => u.Id == userId);
+        private static List<User> users;
+        private string filePath = Path.Combine("Data", "Users.json");
 
-            if (!(user == null))
-            {
-                user.JobCount ++;
+        public UsersController()
+        {
+
+            var jsonString = System.IO.File.ReadAllText(filePath);
+            users = JsonSerializer.Deserialize<List<User>>(jsonString);
+        }
+
+        [HttpGet("{name}/{password}")]
+        public IActionResult Authenticate(string name, string password)
+        {
+            var user = users.FirstOrDefault(u => u.Name.Equals(name) && u.Password.Equals(password));
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public void UpdateUser(int userId)
+        {
+            var user = users.FirstOrDefault(u => u.Id == userId);
+            if (user != null)
+            {           
+
+            user.JobCount++;
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonString = JsonSerializer.Serialize(users, options);
+            System.IO.File.WriteAllText(filePath, jsonString);
 
             }
         }
-        [HttpGet("{userId}")]
-        public IActionResult getJobCount(int userId)
-        {
-            User user = users.FirstOrDefault(u => u.Id == userId);
 
+        [HttpGet("{userId}")]
+        public IActionResult GetJobCount(int userId)
+        {
+            var user = users.FirstOrDefault(u => u.Id == userId);
             if (user == null)
             {
                 return NotFound();
             }
-
             return Ok(user.JobCount);
         }
 
-        // [HttpGet("GetCourseByName")]
-        // public ActionResult GetCourseByName(string name)
-        // {System.Console.WriteLine("gh");
-        //     var course = courses.Find(c => c.name == name);
-        //     if (course == null)
-        //     {
-        //         return NotFound();
-        //     }
-        //     return Ok(course);
-        // }
+       
     }
 }
